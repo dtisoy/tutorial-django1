79e249e9ea88a7570dce25e60630ef44ef840bd1
ltisoy <danieltisoy2001@gmail.com>
2022-04-28 21:31:52 -0500

    paths and getting variables from de url

--- hola/urls.py
+++ hola/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hola, name='hola')
+    path("", views.hola, name='hola'),
+    path("vader/", views.vader, name='hello vader'),
+    path("<str:nombre>/", views.starwars, name="starwars") # getting a variable from the url
 ]
